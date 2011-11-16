7968558d1b28946f3e033ebeaf2ad7bf90484e1f
AaronA <digicyc@gmail.com>
2011-11-16 11:31:29 -0700

    I sway my dotfiles side to side.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/scala/bin:/opt/java//jre/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/scala/bin:/opt/java/jre/bin:/sbin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
