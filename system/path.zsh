910432471e9bd6acf4979f68de60f1884d1732c6
AaronA <digicyc@gmail.com>
2011-11-15 23:21:49 -0700

    personal adjustments

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/scala/bin:/opt/java//jre/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
