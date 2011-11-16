6a55a55ac676067fb52c266a2ac46f0b69a914ef
AaronA <digicyc@gmail.com>
2011-11-15 23:25:33 -0700

    omg more and more like this

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,4 +7,9 @@ then
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+else
+  alias ls="ls -F --color"
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color"
+  alias la='ls -A --color'
 fi
