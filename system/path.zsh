ce7331856ee5fe9aa263f2ebc766ddecee5ac33f
AaronA <digicyc@gmail.com>
2011-11-18 15:19:23 -0700

    Path updates.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/scala/bin:/opt/java/jre/bin:/sbin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/scala/bin:/opt/java/jre/bin:/sbin:/opt/mongodb/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
