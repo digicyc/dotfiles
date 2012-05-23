5214e7eb71d72f7b66069f29166061ef74a46bf7
AaronA <digicyc@gmail.com>
2012-05-23 14:22:27 -0600

    Put prompt back to displaying RVM since now we actually need it for our Ruby Versions.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -77,8 +77,8 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-#export PROMPT=$'\n$(rvm_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
-export PROMPT=$'\n%n@%m in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(rvm_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+#export PROMPT=$'\n%n@%m in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
 }
