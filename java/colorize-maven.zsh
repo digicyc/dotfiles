afc8931f8db1394cfac682d3b510d7b3ac74eb94
AaronA <digicyc@gmail.com>
2012-06-01 14:28:11 -0600

    Added mvn color to our Screen and tmux sessions.

--- java/colorize-maven.zsh
+++ java/colorize-maven.zsh
@@ -72,18 +72,41 @@ ansi_color() {
     c_end=    '[0m'
 }
 
+screen_color() {
+	#	0	Black
+	#	1	Red
+	#	2	Green
+	#	3	Yellow
+	#	4	Blue
+	#	5	Magenta
+	#	6	Cyan
+	#	7	White
+  # \x1b38;5;${i}
+  c_bold=     '\x1b[38;5;15m'
+  c_blue=     '\x1b[38;5;12m'
+  c_black=    '\x1b[38;5;8m'
+  c_green=    '\x1b[38;5;2m'
+  c_magenta=  '\x1b[38;5;13m'
+  c_red=      '\x1b[38;5;9m'
+  c_cyan=     '\x1b[38;5;14m'
+  c_end=      '\x1b[38;5;244m'
+}
+
 color_maven() {
 
-    # pick color type
-    if [ $TERM = 'xterm' ]
-    then
-        xterm_color
-#    elif [ $TERM = 'ansi' ]
-#    then
-#	    ansi_color
-	else
-	    echo "${c_red}WARNING:::Terminal '${TERM}' is not supported at this time. Colorized output will not happen for Maven${c_end}"
-	fi
+  # pick color type
+  if [ $TERM = 'xterm' ]
+  then
+      xterm_color
+  elif [ $TERM = 'screen' ]
+  then
+      xterm_color
+  elif [ $TERM = 'ansi' ]
+  then
+      ansi_color
+  else
+    echo "${c_red}WARNING:::Terminal '${TERM}' is not supported at this time. Colorized output will not happen for Maven${c_end}"
+  fi
 
 	error=${c_bold}${c_red}
 	info=${c_white}
